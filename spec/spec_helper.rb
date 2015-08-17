fd611cd3c4211b0f2c23c93689fbd254cfc38142
Vincent J. DeVendra <VinceDeVendra@gmail.com>
2015-08-17 15:42:58 -0400

    Changed db cleaning config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,6 +44,11 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
